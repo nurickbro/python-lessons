c793db97860a8ee30d29751ffa80724462964deb
nurickbro <117420469+nurickbro@users.noreply.github.com>
2022-11-03 21:07:50 -0700

    codlar

---
+++ codlar.py
@@ -0,0 +1,166 @@
+#a = 20
+#b = 5.5
+#temp = 36.
+#print(type(a))
+#aholi_soni = 7_594_376_567
+#print("aholi soni ", aholi_soni)
+#x, y, z = 16, 4.5, -14
+#c = a*b
+#d = 100//2
+#radius=40
+#PI= 3.4567
+#diametr=2*radius
+#print("Aylana yuzi ",PI*radius )
+#ism="Ikrom"
+#yosh=26
+#xabar=ism + ' ' + str(yosh) + "yoshda"
+#print(xabar)
+#t_yil=int(input("Tug'ilgan yilingizni kiriting: "))
+#yosh= 2022 - t_yil
+#print("siz", yosh, "yoshda ekansiz")
+#a = int("24")
+#b = float(24)
+#temp = str(36.6)
+#a = 20
+#b = -30 
+#c = a + b
+#print(c)
+# Kvadratning yuzini hisoblaymiz
+#kvdrt_tomoni = 20 
+#kvdrt_yuzi = kvdrt_tomoni**2 
+#print(kvdrt_yuzi)
+#pi = 3.14159 
+#radius = 10  
+#diametr = 2*radius
+#print("Aylana uzunligi ", pi*diametr, " ga teng.")
+#Foydalanuvchi kiritgan sonning kvadrati va kubini konsolga chiqaruvchi dastur
+#x = int(input("Istalgan son kiriting:\n>>>"))
+#print(x, " ning kvadrati ", x**2, " ga teng")
+#print(x, " ning kubi ", x**3, " ga teng")
+# Foydalanuvchidan ikki son kiritshni so'rab, {}{{{{[[[{||||}]]]}}}}
+# kiritilgan sonlarning yig'indisi, ayirmasi, 
+# ko'paytmasi va bo'linmasini chiqaruvchi dastur
+#a = float(input("Birinchi sonni kiriting: "))
+#b = float(input("Ikkinchi sonni kiriting: "))
+#c = float(input("Uchinchi sonni kiriting: "))
+#d = int(input("to'rtinchi sonni kiriting: "))
+#print(f"{a}+{b}=", a+b)
+#print(f"{a}-{b}=", a-b)
+#print(f"{a}x{b}=", a*b)
+#print(f"{a}/{b}=", a/b)
+#print(f"{a}/{b}=", a//b+d-c)
+#mevalar = ['olma','shaftoli','anjir',"o'rik"]
+#narxlar = [18000,24000,15000,25600]
+#sonlar =['bir','ikki', 3,4,5]
+#ismlar = []
+#print(mevalar[0].upper())
+#print(mevalar[2].title())
+#print(narxlar[2]+narxlar[1])
+#print(narxlar[3]+1200-25000)
+#print(mevalar.append('qovun'))
+#mevalar[0]=('nok')
+#mevalar.append('tarvuz')
+#mevalar.insert(2,'banan')
+#mevalar.insert(3,'ananas')
+#cars=[]
+#cars.append('Malibu')
+#cars.append('Tracker')
+#cars.append('Nexia 3')
+#cars.append('Lacetti')
+#del cars[2]
+#cars.insert(2,'Jentra')
+#cars.remove('Lacetti')
+#hayvonlar=['it', 'mushuk', 'sigir', "qo'y", 'quyon', 'mushuk']
+#hayvonlar.remove('mushuk')
+#bozorlik=["yog'", 'un', 'piyoz', 'sabzi', "go'sht"]
+#mahsulot = bozorlik.pop(3)
+#print("Men  "+ mahsulot +"  sotib oldim")
+#print("Olinmagan mahsulotlar: ", bozorlik)
+#narxlar[0] = narxlar[0] + 5000
+#t_shaxslar = ['Amir Tumer ', 'Imom Buxoriy ', 'Napoleon']
+#z_shaxslar = ['Bill Gates', 'Ilon Mask', 'Donald Trump' ]
+#print(f"\nMen tarixiy shaxslardan {t_shaxslar.pop(1)} bilan,\n\
+#zamonaviy shaxslardan esa {z_shaxslar.pop(0)} bilan\n\
+#suxbat qilishni istar edim\n")
+#mehmonlar = ['Ikrom', 'Muzappar', 'Xoshimjon', 'Javohir', 'Javhar']
+#for mehmon in mehmonlar:
+#   print("Salom",mehmon)    
+#   print("Hayr,",mehmon)
+#mehmonlar = ['Nozim','Mansur','Davlat','Sardor','Aziz']
+#for mehmon in mehmonlar:
+#   print(f"Hurmatli {mehmon},sizni 20 dekabr kuni oshga taklif qilamiz")
+#    print("Hurmat bilan, Mansurovlar oilasi/n")    
+#sonlar = list(range(1,11))
+#for son in sonlar:
+#    print(f"{son} ning kvadrati {son**2}ga teng")
+#sonlar = list(range(11))
+#sonlar_kvadrati = []
+#for son in sonlar:
+#    sonlar_kvadrati.append(son**2)
+#print(sonlar)
+#print(sonlar_kvadrati)
+#dostlar = []
+#print("5 ta eng yaqin do'stingiz kim?")    
+#for n in range(5):
+#    dostlar.append(input(f"{n+1}-do'stingizning ismini kiriting:  "))
+#print(dostlar)    
+#avtolar = ['audi','bmw','kia','hyundai','volvo']
+#for avto in avtolar:
+#    if avto =='bmw':
+#        print(avto.upper())
+#    else:
+#         print(avto.title())
+#ism = 'Ali'
+#ism.lower() == 'ali'
+#ism = input('ismingiz nima?\n>>')
+#if ism.lower() != 'ali':
+#    print(f"Uzr,{ism.title()} biz Alini kutyapmiz.")
+#else:
+#    print("Salom Ali")
+#javob = float(input("12x6 nechiga teng?>>>"))
+#if javob!=72:
+#    print("Javob xato")
+#yosh=int(input("Yoshingiz nechida?>>>"))
+#if yosh>=18:
+#    print('Xush kelibsiz')
+#else:
+#    print("Kirish mumkin emas!")
+#login = input("Yangi login tanlang:")
+#if len (login)<=5:
+#    print("Login 5 harfdan ko'proq bo'lishi shart!")
+#yil = int(input("Tug'ilgan yilingizni kiriting:"))
+#if 2020-yil<18:
+#    print(f"Yoshingiz{2020-yil}da ekan.")
+#    print("Kirish mumkin emas!")
+#else:
+#     print("Xush kelibsiz!")    
+#yosh = int(input("Yoshingiz nechida?>>>"))
+#if yosh>65: print("Siz Covid - 19 risk guruhida ekansiz")
+#x,y = 50,38
+#print("x>y") if x>y else print("x<y")
+#login = input("Login kiriting: ")
+#if login.lower() == 'admin':
+#  print("Xush kelibsiz Admin, foydalanuvchilar ro'yxatini ko'rasizmi?")
+#else:
+#  print(f"Xush kelibsiz {login.title()}!")
+#x = float(input("Birinchi sonni kiriting: "))
+#y = float(input("Ikkinchi sonni kiriting:"))
+#if x==y: print(f"Sonlar teng: {x}={y}")
+# son = -3
+# if son<0:
+    # print("Manfiy son")
+# else:
+# print("Musbat son")
+     # son = -3
+     # if son<0:
+         # print("Manfiy son")
+     # else:
+          # print("Musbat son")
+          # yosh = int(input('Yoshingiz nechida? '))
+          # if yosh<=4:
+              # print('Sizga kirish bepul.')
+          # elif yosh<=12:
+              # print("Sizga kirish 5000 so'm.")
+          # else:
+              # print("Sizga kirish 10000 so'm")     
+              
